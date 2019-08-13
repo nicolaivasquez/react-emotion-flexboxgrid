174907ce0047fe11b106058e75509c5a53feb8c9
vasquezn <nicolaichristian.vasquez.external@yoox.com>
2019-08-13 16:16:54 +0100

    fix issue where babelrc is not being read properly when this project is installed as a dependency

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
+const { NODE_ENV } = process.env;
+const modules = NODE_ENV === 'production' ? false : 'commonjs';
+const loose = true;
+
 module.exports = {
-  presets: ['./.babelrc.js']
+  presets: [['env', { loose, modules }], 'react'],
+  plugins: [
+    [
+      'emotion',
+      NODE_ENV === 'production'
+        ? { hoist: true }
+        : { sourceMap: true, autoLabel: true }
+    ],
+    ['transform-react-remove-prop-types', { mode: 'unsafe-wrap' }],
+    'transform-object-rest-spread',
+    ['transform-class-properties', { loose }]
+  ]
 };
